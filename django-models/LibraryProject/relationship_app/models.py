20b7c2d7ddc54610e6568eace068a1ad09ab1d2a
stevens456787 <stevenstrudient@gmail.com>
2025-02-21 22:34:22 +0300

    update the code

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,16 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
-# filepath: /c:/Users/Rnyamari/Desktop/DJ LEARN/Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/views.py
 from bookshelf.models import Book, Library
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
@@ -25,4 +23,11 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
