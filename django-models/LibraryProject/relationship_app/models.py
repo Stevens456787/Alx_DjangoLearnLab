e7b5b258533c30d5be15cf4e96982cee412af73b
stevens456787 <stevenstrudient@gmail.com>
2025-02-21 22:25:12 +0300

    update the code

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+# filepath: /c:/Users/Rnyamari/Desktop/DJ LEARN/Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/views.py
+from bookshelf.models import Book, Library
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
@@ -16,30 +18,11 @@ class UserProfile(models.Model):
 
 # Create your models here.
 
-#class Author(models.Model):
-    #name = models.CharField(max_length=255)
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
 
-    #def __str__(self):
-        #return self.name
-
-#class Book(models.Model):
-    #title = models.CharField(max_length=255)
-    #author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
-
-    #def __str__(self):
-        #return self.title
-
-#class Library(models.Model):
-    #name = models.CharField(max_length=255)
-    #books = models.ManyToManyField(Book, related_name="libraries")
-
-    #def __str__(self):
-        #return self.name
-
-#class Librarian(models.Model):
-    #name = models.CharField(max_length=255)
-    #library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
-
-    #def __str__(self):
-        #return self.name
+    def __str__(self):
+        return self.title
 
