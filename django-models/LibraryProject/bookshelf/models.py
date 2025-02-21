d7b6b383bfbb75f319386dde7a6d08d6ac4f6e32
stevens456787 <stevenstrudient@gmail.com>
2025-02-21 22:30:02 +0300

    update the code

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -9,3 +9,13 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+# filepath: /c:/Users/Rnyamari/Desktop/DJ LEARN/Alx_DjangoLearnLab/django-models/LibraryProject/bookshelf/models.py
+from django.db import models
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
