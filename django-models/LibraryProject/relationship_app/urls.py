9ed99513e9ec93d00fe82f057a0de81500f47bc6
stevens456787 <stevenstrudient@gmail.com>
2025-02-22 14:49:54 +0300

    update the code

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     # Authentication URLs
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', auth_views.register, name='register'),
     
     #path('admin-dashboard/',name='admin_dashboard'),
     #path('librarian-dashboard/',name='librarian_dashboard'),
