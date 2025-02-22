8311a1309cf23c763b5253f64ac7155ff1df8098
stevens456787 <stevenstrudient@gmail.com>
2025-02-22 13:21:38 +0300

    update the code

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required, permission_required
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.views import View
 from .models import Book
 from .models import Library# Ensure Library is imported here
