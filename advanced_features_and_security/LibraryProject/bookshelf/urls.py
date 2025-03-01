66a894efc35edc4f3c839140d440acfc1bd57d96
stevens456787 <stevenstrudient@gmail.com>
2025-03-01 16:37:43 +0300

    update code

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import search_books
 
 urlpatterns = [
+    
+    
     path('search/', search_books, name='search_books'),
+    path('add/', add_book, name='add_book'),
 ]
