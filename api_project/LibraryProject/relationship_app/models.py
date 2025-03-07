ee8db6b164aa832d3e016284e9a4454ff3376b81
stevens456787 <stevenstrudient@gmail.com>
2025-03-07 20:46:12 +0300

    update dir

--- api_project/LibraryProject/relationship_app/models.py
+++ api_project/LibraryProject/relationship_app/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+
 
 # Define Author model first to avoid reference issues
 class Author(models.Model):
