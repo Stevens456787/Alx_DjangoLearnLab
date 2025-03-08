64774a6b5bdd9aca66d67a52699098daad9cf1f6
stevens456787 <stevenstrudient@gmail.com>
2025-03-08 10:07:19 +0300

    update code of the dir

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,12 +2,13 @@
 
 # Create your views here.
 from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from rest_framework.generics import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
