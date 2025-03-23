fb23303bf2fac74397e702407b76622797addacd
stevens456787 <stevenstrudient@gmail.com>
2025-03-23 12:34:15 +0300

    new files for blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment_create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
-    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
