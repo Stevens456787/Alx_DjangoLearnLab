ff881bd17e369a84d4ed39404a4eaf5e1a774d82
stevens456787 <stevenstrudient@gmail.com>
2025-03-23 12:41:10 +0300

    new files for blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -12,4 +13,12 @@ class CommentForm(forms.ModelForm):
         fields = ["content"]
         widgets = {
             "content": forms.Textarea(attrs={'class': 'form-control', "rows": 3}),
+        }
+        
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "content", "tags"]
+        widgets = {
+            "tags": TagWidget(),
         }
