fa930ede64a9f0c537acd7816504aae1c58fc12a
stevens456787 <stevenstrudient@gmail.com>
2025-03-23 12:23:38 +0300

    new files for blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from .models import Post
-
+from .models import Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
+        widget = {
+            "content": forms.Textarea(attrs={"class": "form-control"})
+        }
