8a04698c6ab0f95243fb144c5568e29d10011a83
stevens456787 <stevenstrudient@gmail.com>
2025-03-28 21:34:15 +0300

    update models.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
 User = get_user_model() 
 
-class UseSerializerrSerializer(serializers.ModelSerializer):
+class UseSerializerSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'name', 'email', 'bio', 'profile_picture','followers']
