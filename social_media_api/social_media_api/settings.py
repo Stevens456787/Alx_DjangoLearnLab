c5a02dfb8669076936ba6e39f24cf3ee9e982ef9
stevens456787 <stevenstrudient@gmail.com>
2025-03-29 11:35:51 +0300

    dedug settings.py set to false

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3cmdgz+gs_9frcpm#g=v@n4gq3_posevqg6!+@1+v%84^(9^%9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
