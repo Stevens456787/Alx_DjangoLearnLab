c5ea4b76a030b9e5a0a0d52b414e8a256e070c00
stevens456787 <stevenstrudient@gmail.com>
2025-03-29 11:05:42 +0300

    update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,36 +1,34 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 
+User = get_user_model()
 
-User = get_user_model() 
-
-class UseSerializerSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'bio', 'profile_picture','followers']
-        
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        User = get_user_model()  # Explicitly call get_user_model
-        user = User.objects.create_user(**validated_data)
-        Token.objects.create(user=user)  # Create a token for the new user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
-    
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
 
-    def validate(self, attrs):
-        user = authenticate(username=attrs['username'], password=attrs['password'])
-        if user:
-            return {'user': user}
-        raise serializers.ValidationError("Invalid credentials")
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
