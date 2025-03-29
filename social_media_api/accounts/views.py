97b5ce2444f403696ae68b4f4674d8e2b6af5cc3
stevens456787 <stevenstrudient@gmail.com>
2025-03-29 11:17:45 +0300

    update the views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,32 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token 
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status, generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import AllowAny
 
 # Create your views here.
 
 User = get_user_model()
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def register(request):
-    if request.method == 'POST':
-        serializer = RegisterSerializer(data=request.data)
+class RegisterView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             token = Token.objects.create(user=user)
             return Response({"user": serializer.data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def login(request):
-    if request.method == 'POST':
+
+
+class LoginView(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(username=username, password=password)
@@ -34,3 +35,12 @@ def login(request):
             token, created = Token.objects.get_or_create(user=user)
             return Response({"token": token.key}, status=status.HTTP_200_OK)
         return Response({"error": "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class UserListView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        users = User.objects.all()
+        user_data = [{"id": user.id, "username": user.username, "email": user.email} for user in users]
+        return Response(user_data, status=status.HTTP_200_OK)
