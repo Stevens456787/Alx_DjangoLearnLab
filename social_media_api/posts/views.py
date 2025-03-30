30d14f6fd0f74c1f343ea1fa219021d0bf113324
stevens456787 <stevenstrudient@gmail.com>
2025-03-30 20:06:16 +0300

    update posts views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, permissions, filters, status, generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.shortcuts import generics, get_object_or_404
@@ -7,72 +9,53 @@ from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 from accounts.models import CustomUser
 from notifications.models import Notification
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        user_to_follow = CustomUser.objects.get(id=user_id)
+    except CustomUser.DoesNotExist:
+        return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    user = request.user
 
-    @action(detail=True, methods=['post'])
-    def comment(self, request, pk=None):
-        post = generics.get_object_or_404(Post, pk=pk)
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(post=post, author=request.user)
-            # Create notification
-            Notification.objects.create(
-                recipient=post.author,
-                actor=request.user,
-                verb='commented on your post',
-                target=post,
-            )
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if user == user_to_follow:
+        return Response({'detail': 'You cannot follow yourself'}, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=True, methods=['post'])
-    def like(self, request, pk=None):
-        post = generics.get_object_or_404(Post, pk=pk)
-        like, created = Like.objects.get_or_create(user=request.user, post=post)
-        if created:
-            # Create notification
-            Notification.objects.create(
-                recipient=post.author,
-                actor=request.user,
-                verb='liked your post',
-                target=post,
-            )
-            return Response({'status': 'liked'}, status=status.HTTP_201_CREATED)
-        return Response({'status': 'already liked'}, status=status.HTTP_200_OK)
+    # Add the user to the following list
+    user.following.add(user_to_follow)
+    user_to_follow.followers.add(user)
 
-    @action(detail=True, methods=['post'])
-    def unlike(self, request, pk=None):
-        post = generics.get_object_or_404(Post, pk=pk)
-        like = generics.get_object_or_404(Like, user=request.user, post=post)
-        like.delete()
-        return Response({'status': 'unliked'}, status=status.HTTP_200_OK)
+    return Response({'detail': 'Followed successfully'}, status=status.HTTP_200_OK)
 
-    @action(detail=False, methods=['get'])
-    def feed(self, request):
-        following_users = request.user.followers.all()
-        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
-        page = self.paginate_queryset(posts)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-        serializer = self.get_serializer(posts, many=True)
-        return Response(serializer.data)
+#unfollow user function
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        user_to_unfollow = CustomUser.objects.get(id=user_id)
+    except CustomUser.DoesNotExist:
+        return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    user = request.user
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    if user == user_to_unfollow:
+        return Response({'detail': 'You cannot unfollow yourself'}, status=status.HTTP_400_BAD_REQUEST)
 
-    def perform_update(self, serializer):
-        serializer.save(author=self.request.user)
+    # Remove the user from the following list
+    user.following.remove(user_to_unfollow)
+    user_to_unfollow.followers.remove(user)
+
+    return Response({'detail': 'Unfollowed successfully'}, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def user_feed(request):
+    # user = CustomUser
+    user = request.user    
+    # Get posts from the users the current user is following
+    followed_users = user.following.all()
+    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
